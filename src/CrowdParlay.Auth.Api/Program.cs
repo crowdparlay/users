27aa9e80510c4b8786b6976736eaeb087932d9bf
undrcrxwn <undrcrxwn@gmail.com>
2023-04-05 18:12:03 +0300

    Hotfix CORS

--- src/CrowdParlay.Auth.Api/Program.cs
+++ src/CrowdParlay.Auth.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using CrowdParlay.Auth.Api.Extensions;
 using CrowdParlay.Auth.Application.Extensions;
 using CrowdParlay.Auth.Infrastructure.Extensions;
@@ -17,6 +18,23 @@ builder.Services.AddSwaggerGen();
 builder.Host.UseSerilog();
 
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+    context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+    context.Response.Headers.Add("Access-Control-Allow-Headers",
+        "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Accept-Encoding, Content-Length, Content-MD5, Date, X-Api-Version, X-File-Name");
+    context.Response.Headers.Add("Access-Control-Allow-Methods", "POST,GET,PUT,PATCH,DELETE,OPTIONS");
+
+    if (context.Request.Method == "OPTIONS")
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.OK;
+        await context.Response.WriteAsync(string.Empty);
+    }
+
+    await next(context);
+});
 
 if (app.Environment.IsDevelopment())
 {
