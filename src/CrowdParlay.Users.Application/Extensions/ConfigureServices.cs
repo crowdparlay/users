5b25b00431db27ba8494784f54b97c2aec167cd9
undrcrxwn <undrcrxwn@gmail.com>
2023-06-09 18:51:57 +0300

    fix(di): register password hasher

--- src/CrowdParlay.Users.Application/Extensions/ConfigureServices.cs
+++ src/CrowdParlay.Users.Application/Extensions/ConfigureServices.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
+using CrowdParlay.Users.Application.Abstractions;
 using CrowdParlay.Users.Application.Behaviors;
+using CrowdParlay.Users.Application.Services;
 using FluentValidation;
 using Mapster;
 using Mediator;
@@ -13,8 +15,9 @@ public static class ConfigureServices
     {
         var assembly = Assembly.GetExecutingAssembly();
         TypeAdapterConfig.GlobalSettings.Scan(assembly);
-        
+
         return services
+            .AddSingleton<IPasswordHasher, PasswordHasher>()
             .AddValidatorsFromAssembly(assembly)
             .AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>))
             .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>))
