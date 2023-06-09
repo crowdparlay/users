ec068fa300e5ff4202228dfcf7c9866de144b2df
undrcrxwn <undrcrxwn@gmail.com>
2023-06-09 19:11:08 +0300

    fix(ef core): add EF Core context for OpenIddict

--- src/CrowdParlay.Users.Infrastructure.Persistence/Extensions/ConfigureServices.cs
+++ src/CrowdParlay.Users.Infrastructure.Persistence/Extensions/ConfigureServices.cs
@@ -3,6 +3,7 @@ using CrowdParlay.Users.Application.Services;
 using CrowdParlay.Users.Domain.Abstractions;
 using CrowdParlay.Users.Infrastructure.Persistence.Abstractions;
 using CrowdParlay.Users.Infrastructure.Persistence.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CrowdParlay.Users.Infrastructure.Persistence.Extensions;
@@ -11,19 +12,18 @@ public static class ConfigureServices
 {
     public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services)
     {
-        services.AddSingleton<IDbConnectionFactory>(_ =>
-        {
-            var connectionString = Environment.ExpandEnvironmentVariables(@"
-                Host     = %POSTGRES_HOST%;
-                Port     = %POSTGRES_PORT%;
-                Database = %POSTGRES_DB%;
-                Username = %POSTGRES_USER%;
-                Password = %POSTGRES_PASSWORD%");
-
-            return new SqlConnectionFactory(connectionString);
-        });
+        var connectionString = Environment.ExpandEnvironmentVariables(
+            """
+            Host     = %POSTGRES_HOST%;
+            Port     = %POSTGRES_PORT%;
+            Database = %POSTGRES_DB%;
+            Username = %POSTGRES_USER%;
+            Password = %POSTGRES_PASSWORD%
+            """);
 
         return services
+            .AddDbContext<OpenIddictDbContext>(options => options.UseNpgsql(connectionString))
+            .AddSingleton<IDbConnectionFactory>(new SqlConnectionFactory(connectionString))
             .AddScoped<IUsersRepository, UsersRepository>()
             .AddScoped<IAuthenticationService, AuthenticationService>()
             .AddHostedService<DataStoreInitializer>();
--- src/CrowdParlay.Users.Infrastructure.Persistence/OpenIddictDbContext.cs
+++ src/CrowdParlay.Users.Infrastructure.Persistence/OpenIddictDbContext.cs
@@ -2,4 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CrowdParlay.Users.Infrastructure.Persistence;
 
-public class OpenIddictDbContext : DbContext { }
+public class OpenIddictDbContext : DbContext
+{
+    public OpenIddictDbContext(DbContextOptions<OpenIddictDbContext> options) : base(options) { }
+}
