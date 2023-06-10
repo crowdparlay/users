66bdb32021c676b052d549f8be44adf6dee61c33
undrcrxwn <undrcrxwn@gmail.com>
2023-06-10 19:14:42 +0300

    fix(openid): rewrite OpenIddict refresh token flow

--- src/CrowdParlay.Users.Application/Features/Authentication/Commands/ExchangeRefreshToken.cs
+++ src/CrowdParlay.Users.Application/Features/Authentication/Commands/ExchangeRefreshToken.cs
@@ -2,46 +2,33 @@ using System.Security.Claims;
 using CrowdParlay.Users.Application.Extensions;
 using CrowdParlay.Users.Application.Exceptions;
 using CrowdParlay.Users.Domain.Abstractions;
+using Dodo.Primitives;
 using FluentValidation;
 using Mediator;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Tokens;
 using OpenIddict.Abstractions;
 using OpenIddict.Server.AspNetCore;
-using IAuthenticationService = CrowdParlay.Users.Application.Abstractions.IAuthenticationService;
 
 namespace CrowdParlay.Users.Application.Features.Authentication.Commands;
 
 public static class ExchangeRefreshToken
 {
-    public sealed record Command(string Username, string Scope, HttpContext Context) : IRequest<Response>;
-
-    public sealed class Validator : AbstractValidator<Command>
-    {
-        public Validator() => RuleFor(x => x.Username).NotEmpty();
-    }
+    public sealed record Command(Uuid UserId, string Scope) : IRequest<Response>;
 
     public sealed class Handler : IRequestHandler<Command, Response>
     {
         private readonly IUsersRepository _users;
-        private readonly IAuthenticationService _authenticationService;
 
-        public Handler(IUsersRepository users, IAuthenticationService authenticationService)
-        {
-            _users = users;
-            _authenticationService = authenticationService;
-        }
+        public Handler(IUsersRepository users) => _users = users;
 
         public async ValueTask<Response> Handle(Command request, CancellationToken cancellationToken)
         {
             var user =
-                await _users.GetByUsernameAsync(request.Username)
+                await _users.GetByIdAsync(request.UserId)
                 ?? throw new NotFoundException("The user does not exist.");
 
-            var authenticateResult = await request.Context.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
             var identity = new ClaimsIdentity(
-                claims: authenticateResult.Principal!.Claims,
                 authenticationType: TokenValidationParameters.DefaultAuthenticationType,
                 nameType: OpenIddictConstants.Claims.Name,
                 roleType: OpenIddictConstants.Claims.Role);
