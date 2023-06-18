fd0ce6179c3105a6f90d0ddd1ad0dee5e1c5977f
undrcrxwn <undrcrxwn@gmail.com>
2023-06-18 17:47:50 +0300

    refactor(kafka): throw exception if the required KAFKA_PRODUCER_NAME configuration value is missing

--- src/CrowdParlay.Users.Infrastructure.Communication/Services/KafkaMessageBroker.cs
+++ src/CrowdParlay.Users.Infrastructure.Communication/Services/KafkaMessageBroker.cs
@@ -10,7 +10,10 @@ public sealed class KafkaMessageBroker : IMessageBroker
 
     public KafkaMessageBroker(IConfiguration configuration, IProducerAccessor producerAccessor)
     {
-        var producerName = configuration["KAFKA_PRODUCER_NAME"]!;
+        var producerName =
+            configuration["KAFKA_PRODUCER_NAME"]
+            ?? throw new InvalidOperationException("Missing required configuration 'KAFKA_PRODUCER_NAME'");
+
         var producer = producerAccessor.GetProducer(producerName);
 
         UserEvents = new KafkaTopic("user-events", producer);
