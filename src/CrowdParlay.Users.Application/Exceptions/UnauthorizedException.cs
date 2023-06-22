da8a01856b4522c3e4c7f407639afcc4e9f506e9
Akavi <gabdushev.i@yandex.ru>
2023-06-22 15:19:41 +0500

    feat(exceptions): add 401 exception

---
+++ src/CrowdParlay.Users.Application/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,13 @@
+﻿namespace CrowdParlay.Users.Application.Exceptions;
+
+public class UnauthorizedException : Exception
+{
+    public UnauthorizedException()
+        : base("") { }
+
+    public UnauthorizedException(string message)
+        : base(message) { }
+
+    public UnauthorizedException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
