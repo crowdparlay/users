4b2210e0ee5d37becac955acf4943e18b70ba3c5
undrcrxwn <undrcrxwn@gmail.com>
2023-06-28 23:51:11 +0300

    chore: remove Communication project into separate NuGet

--- src/CrowdParlay.Users.Application/Abstractions/Communication/IMessageDestination.cs
+++
@@ -1,6 +0,0 @@
-namespace CrowdParlay.Users.Application.Abstractions.Communication;
-
-public interface IMessageDestination
-{
-    public void Publish(object message);
-}
--- src/CrowdParlay.Users.Application/Communication/Messages.cs
+++
@@ -1,5 +0,0 @@
-namespace CrowdParlay.Users.Application.Communication;
-
-public record UserCreatedEvent(string UserId, string Username, string DisplayName);
-public record UserUpdatedEvent(string UserId, string Username, string DisplayName);
-public record UserDeletedEvent(string UserId);
