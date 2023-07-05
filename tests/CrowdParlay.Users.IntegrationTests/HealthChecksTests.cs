b71a40c0547ae16cd604e05737d8465872a14bd7
maxay <Maxaytt@gmail.com>
2023-07-05 15:49:45 +0200

    tests: add DatabaseAvailable test for HealthCheck

---
+++ tests/CrowdParlay.Users.IntegrationTests/HealthChecksTests.cs
@@ -0,0 +1,23 @@
+using CrowdParlay.Communication.RabbitMq;
+using CrowdParlay.Users.IntegrationTests.Attribute;
+using CrowdParlay.Users.IntegrationTests.Setups;
+using FluentAssertions;
+
+namespace CrowdParlay.Users.IntegrationTests;
+
+public class HealthChecksTests
+{
+    [Theory(Timeout = 5000), Setups(typeof(TestContainersSetup), typeof(ServerSetup))]
+    public async Task HealthCheck_DatabaseAvailable_ShouldBeHealthy(HttpClient client)
+    {
+        // Arrange
+        
+        // Act
+        var response = await client.GetAsync("/health");
+        
+        // Assert
+        var content = await response.Content.ReadAsStringAsync();
+        response.EnsureSuccessStatusCode();
+        content.Should().Contain("Healthy");
+    }
+}
