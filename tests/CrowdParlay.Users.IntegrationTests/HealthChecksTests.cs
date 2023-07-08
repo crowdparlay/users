b1adb20cb3ba1f6002f796b8a5a20552d4aacce6
undrcrxwn <undrcrxwn@gmail.com>
2023-07-08 19:19:00 +0300

    tests: add negative health checks test

--- tests/CrowdParlay.Users.IntegrationTests/HealthChecksTests.cs
+++ tests/CrowdParlay.Users.IntegrationTests/HealthChecksTests.cs
@@ -18,4 +18,19 @@ public class HealthChecksTests
         response.Should().HaveStatusCode(HttpStatusCode.OK);
         content.Should().BeEquivalentTo("healthy");
     }
+    
+    [Theory(Timeout = 5000), ApiSetup]
+    public async Task HealthCheck_DatabaseNotAvailable_ShouldBeUnhealthy(HttpClient client, PostgreSqlContainer postgres)
+    {
+        // Arrange
+        await postgres.StopAsync();
+        
+        // Act
+        var response = await client.GetAsync("/health");
+        
+        // Assert
+        response.Should().HaveStatusCode(HttpStatusCode.ServiceUnavailable);
+        var content = await response.Content.ReadAsStringAsync();
+        content.Should().BeEquivalentTo("unhealthy");
+    }
 }
