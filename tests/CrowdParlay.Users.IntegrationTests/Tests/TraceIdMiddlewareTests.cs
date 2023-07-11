049e6d0c434171b677f47eaec222c589136f711b
maxay <Maxaytt@gmail.com>
2023-07-11 21:35:01 +0200

    refactor(tests): rename trace ID test and make test shorter with PostAsJsonAsync

--- tests/CrowdParlay.Users.IntegrationTests/Tests/TraceIdMiddlewareTests.cs
+++ tests/CrowdParlay.Users.IntegrationTests/Tests/TraceIdMiddlewareTests.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Json;
 using System.Text;
 using CrowdParlay.Users.Application.Features.Users.Commands;
 using CrowdParlay.Users.IntegrationTests.Attributes;
@@ -9,14 +10,13 @@ namespace CrowdParlay.Users.IntegrationTests.Tests;
 public class TraceIdMiddlewareTests
 {
     [Theory(Timeout = 5000), ApiSetup]
-    public async Task ResponseCheck_CommonResponse_ShouldContainTraceId(HttpClient client)
+    public async Task SuccessResponse_ShouldContain_TraceIdHeader(HttpClient client)
     {
         // Arrange
         var command = new Register.Command("username", "display name", "password", false);
-        var jsonContent = new StringContent(JsonConvert.SerializeObject(command), Encoding.UTF8, "application/json");
-        
+
         // Act
-        var response = await client.PostAsync("/api/users/register", jsonContent);
+        var response = await client.PostAsJsonAsync("/api/users/register", command);
         
         // Assert
         response.Headers.Should().Contain(header => header.Key == "X-TraceId");
