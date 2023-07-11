0cb7d72d92bfba2c140ed625d892dfee349e2014
maxay <Maxaytt@gmail.com>
2023-07-11 22:35:01 +0200

    tests: add failureResponse and MultipleResponses tests

--- tests/CrowdParlay.Users.IntegrationTests/Tests/TraceIdMiddlewareTests.cs
+++ tests/CrowdParlay.Users.IntegrationTests/Tests/TraceIdMiddlewareTests.cs
@@ -9,6 +9,8 @@ namespace CrowdParlay.Users.IntegrationTests.Tests;
 
 public class TraceIdMiddlewareTests
 {
+    private const string TraceIdHeaderName = "X-TraceId";
+
     [Theory(Timeout = 5000), ApiSetup]
     public async Task SuccessResponse_ShouldContain_TraceIdHeader(HttpClient client)
     {
@@ -20,5 +22,40 @@ public class TraceIdMiddlewareTests
         
         // Assert
         response.Headers.Should().Contain(header => header.Key == "X-TraceId");
+    }
+
+    [Theory(Timeout = 5000), ApiSetup]
+    public async Task FailureResponse_ShouldContain_TraceIdHeader(HttpClient client)
+    {
+        // Arrange
+        var command = new Register.Command(string.Empty, string.Empty, string.Empty, false);
+
+        // Act
+        var response = await client.PostAsJsonAsync("/api/users/register", command);
+        
+        // Assert
+        response.Headers.Should().Contain(header => header.Key == TraceIdHeaderName);
+        response.Should().HaveClientError();
+    }
+
+    [Theory(Timeout = 5000), ApiSetup]
+    public async Task MultipleResponses_ShouldContain_UniqueTraceIdHeaders(HttpClient client)
+    {
+        // Arrange
+        var command = new Register.Command("username", "display name", "password", false);
+
+        // Act
+        var postResponse = await client.PostAsJsonAsync("/api/users/register", command);
+        var errorResponse = await client.PostAsJsonAsync("/api/users/register", command);
+
+        // Assert
+        postResponse.Headers.Should().Contain(header => header.Key == TraceIdHeaderName);
+        errorResponse.Headers.Should().Contain(header => header.Key == TraceIdHeaderName);
+        errorResponse.Should().HaveClientError();
+
+        var postTraceId = postResponse.Headers.GetValues(TraceIdHeaderName).ToList();
+        var errorTraceId = errorResponse.Headers.GetValues(TraceIdHeaderName).ToList();
+
+        postTraceId.Should().NotContainEquivalentOf(errorTraceId);
     }
 }
