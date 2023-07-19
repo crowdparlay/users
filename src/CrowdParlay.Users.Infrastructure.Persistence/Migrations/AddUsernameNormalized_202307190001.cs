11827ddf5669102ccf7e53d4b68f4b93edcaaeac
undrcrxwn <undrcrxwn@gmail.com>
2023-07-19 22:24:09 +0300

    feat(migrations): add normalized username column to users table

---
+++ src/CrowdParlay.Users.Infrastructure.Persistence/Migrations/AddUsernameNormalized_202307190001.cs
@@ -0,0 +1,10 @@
+using FluentMigrator;
+
+namespace CrowdParlay.Users.Infrastructure.Persistence.Migrations;
+
+[Migration(202307190001)]
+public class AddUsernameNormalized_202307190001 : Migration
+{
+    public override void Up() => Create.Column("username_normalized").OnTable("users").AsString().Nullable();
+    public override void Down() => Delete.Column("avatar_url").FromTable("users");
+}
