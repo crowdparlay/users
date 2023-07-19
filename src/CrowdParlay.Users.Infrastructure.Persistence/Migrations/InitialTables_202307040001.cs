41f3415984a40219abe453b857795c26fccf7165
undrcrxwn <undrcrxwn@gmail.com>
2023-07-19 22:23:36 +0300

    refactor(migrations): reorder methods

--- src/CrowdParlay.Users.Infrastructure.Persistence/Migrations/InitialTables_202307040001.cs
+++ src/CrowdParlay.Users.Infrastructure.Persistence/Migrations/InitialTables_202307040001.cs
@@ -5,12 +5,6 @@ namespace CrowdParlay.Users.Infrastructure.Persistence.Migrations;
 [Migration(202307040001)]
 public class InitialTables_202307040001 : Migration
 {
-    public override void Down()
-    {
-        IfDatabase("Postgres").Execute.Sql("""DROP EXTENSION "uuid-ossp";""");
-        Delete.Table("users");
-    }
-    
     public override void Up()
     {
         IfDatabase("Postgres").Execute.Sql("""CREATE EXTENSION "uuid-ossp";""");
@@ -20,5 +14,11 @@ public class InitialTables_202307040001 : Migration
             .WithColumn("display_name").AsString(50).NotNullable()
             .WithColumn("password_hash").AsString(100).NotNullable()
             .WithColumn("created_at").AsDateTimeOffset().NotNullable();
+    }
+    
+    public override void Down()
+    {
+        IfDatabase("Postgres").Execute.Sql("""DROP EXTENSION "uuid-ossp";""");
+        Delete.Table("users");
     }
 }
