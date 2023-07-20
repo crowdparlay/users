06562865ce5d87428ba6826cd8e0995d2a9acfc2
maxay <Maxaytt@gmail.com>
2023-07-20 12:18:49 +0200

    fix(migrations): changed "avatar_url" to "username_normalized"

--- src/CrowdParlay.Users.Infrastructure.Persistence/Migrations/AddUsernameNormalized_202307190001.cs
+++ src/CrowdParlay.Users.Infrastructure.Persistence/Migrations/AddUsernameNormalized_202307190001.cs
@@ -6,5 +6,5 @@ namespace CrowdParlay.Users.Infrastructure.Persistence.Migrations;
 public class AddUsernameNormalized_202307190001 : Migration
 {
     public override void Up() => Create.Column("username_normalized").OnTable("users").AsString().Nullable();
-    public override void Down() => Delete.Column("avatar_url").FromTable("users");
+    public override void Down() => Delete.Column("username_normalized").FromTable("users");
 }
