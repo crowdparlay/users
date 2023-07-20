892b1ce58ec64ec2998453b07d3417711c7f751b
maxay <Maxaytt@gmail.com>
2023-07-20 13:49:31 +0200

    feat(migrations): add normalized username index

--- src/CrowdParlay.Users.Infrastructure.Persistence/Migrations/AddUsernameNormalized_202307190001.cs
+++ src/CrowdParlay.Users.Infrastructure.Persistence/Migrations/AddUsernameNormalized_202307190001.cs
@@ -5,6 +5,17 @@ namespace CrowdParlay.Users.Infrastructure.Persistence.Migrations;
 [Migration(202307190001)]
 public class AddUsernameNormalized_202307190001 : Migration
 {
-    public override void Up() => Create.Column("username_normalized").OnTable("users").AsString().Nullable();
-    public override void Down() => Delete.Column("username_normalized").FromTable("users");
+    public override void Up()
+    {
+        Create.Column("username_normalized").OnTable("users").AsString().Nullable();
+
+        Create.Index("UQ_username_normalized").OnTable("users").OnColumn("username_normalized").Unique();
+    }
+
+    public override void Down()
+    {
+        Delete.Index("UQ_username_normalized").OnTable("users");
+        
+        Delete.Column("username_normalized").FromTable("users");
+    }
 }
