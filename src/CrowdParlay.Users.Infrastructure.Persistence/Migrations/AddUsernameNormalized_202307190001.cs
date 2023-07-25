d92764055a42d3f7e4083ebdaba10ef88d5509f8
maxay <Maxaytt@gmail.com>
2023-07-25 21:32:39 +0200

    fix: remove unnecessary unique and drop function

--- src/CrowdParlay.Users.Infrastructure.Persistence/Migrations/AddUsernameNormalized_202307190001.cs
+++ src/CrowdParlay.Users.Infrastructure.Persistence/Migrations/AddUsernameNormalized_202307190001.cs
@@ -8,22 +8,26 @@ public class AddUsernameNormalized_202307190001 : Migration
 {
     public override void Up()
     {
-        Create.Column("username_normalized").OnTable("users").AsString().Nullable().Unique();
+        Create.Column("username_normalized").OnTable("users").AsString(25);
         
         Execute.Sql(StoredFunction);
         Execute.Sql(TriggerFunction);
         Execute.Sql(Trigger);
+        
+        Create.Index("users_username_normalized_idx").OnTable("users").OnColumn("username_normalized").Unique();
     }
 
     public override void Down()
     {
-        Delete.Index("UQ_username_normalized").OnTable("users");
+        Delete.Index("users_username_normalized_idx").OnTable("users");
         
         Delete.Column("username_normalized").FromTable("users");
         
         Execute.Sql("DROP TRIGGER IF EXISTS normalize_username_trigger ON users;");
 
-        Execute.Sql("DROP FUNCTION IF EXISTS normalize_username(NVARCHAR)");
+        Execute.Sql("DROP FUNCTION IF EXISTS normalize_username(TEXT)");
+        
+        Execute.Sql("DROP FUNCTION IF EXISTS normalize_username_trigger_function()");
     }
 
     
