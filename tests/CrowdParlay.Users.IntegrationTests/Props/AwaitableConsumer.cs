00ec1739c0e1e990f9e4fc719f13c53a3d658b2f
undrcrxwn <undrcrxwn@gmail.com>
2023-07-29 21:43:48 +0300

    tests: make awaitable consumer thread-safe

--- tests/CrowdParlay.Users.IntegrationTests/Props/AwaitableConsumer.cs
+++ tests/CrowdParlay.Users.IntegrationTests/Props/AwaitableConsumer.cs
@@ -1,21 +1,12 @@
+using System.Threading.Channels;
 using CrowdParlay.Communication.Abstractions;
 
 namespace CrowdParlay.Users.IntegrationTests.Props;
 
 public class AwaitableConsumer<TMessage> : IMessageListener<TMessage> where TMessage : Message
 {
-    private TaskCompletionSource<TMessage> _tcs = new();
+    private readonly Channel<Message> _channel = Channel.CreateUnbounded<Message>();
 
-    public async Task<TMessage> ConsumeOne()
-    {
-        var message = await _tcs.Task;
-        _tcs = new TaskCompletionSource<TMessage>();
-        return message;
-    }
-
-    public Task HandleAsync(TMessage message)
-    {
-        _tcs.SetResult(message);
-        return Task.CompletedTask;
-    }
+    public async Task<Message> ConsumeOne() => await _channel.Reader.ReadAsync();
+    public async Task HandleAsync(TMessage message) => await _channel.Writer.WriteAsync(message);
 }
