df6bcf7f7369c48ee26e9cc13bc52c5c26c37704
undrcrxwn <undrcrxwn@gmail.com>
2023-10-05 16:05:34 +0300

    chore: replace batch scripts with .NET tools

--- scripts/swagger_spec.bat
+++
@@ -1,9 +0,0 @@
-setlocal EnableDelayedExpansion
-
-set dll=%1
-set ver=%2
-set dst=spec\swagger\%ver%\
-
-mkdir %dst%
-swagger tofile --output %dst%swagger.json        %dll% %ver%
-swagger tofile --output %dst%swagger.yaml --yaml %dll% %ver%
