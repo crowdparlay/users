53bb2c4960bdf50d014412b5746949abc09b45ff
undrcrxwn <undrcrxwn@gmail.com>
2023-10-14 22:10:11 +0300

    fix: apply EF Core migrations on database initialization

--- src/CrowdParlay.Users.Infrastructure.Persistence/Migrations/EfCore/OpenIddictDbContextModelSnapshot.cs
+++ src/CrowdParlay.Users.Infrastructure.Persistence/Migrations/EfCore/OpenIddictDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace CrowdParlay.Users.Infrastructure.Persistence.Migrations.EfCore
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.5")
+                .HasAnnotation("ProductVersion", "7.0.12")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
