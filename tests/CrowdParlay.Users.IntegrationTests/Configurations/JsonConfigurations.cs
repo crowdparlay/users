89be3da44358ba5e3e45949db43fe653bf016011
maxaytt <Maxaytt@gmail.com>
2023-10-20 19:12:35 +0200

    fix(tests): add access_token to tests

---
+++ tests/CrowdParlay.Users.IntegrationTests/Configurations/JsonConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using CrowdParlay.Users.Api.Services;
+
+namespace CrowdParlay.Users.IntegrationTests.Configurations;
+
+public static class JsonConfigurations
+{
+    public static JsonSerializerOptions JsonOptions { get; } = new()
+    {
+        PropertyNamingPolicy = new SnakeCaseJsonNamingPolicy(), 
+        DictionaryKeyPolicy = new SnakeCaseJsonNamingPolicy() 
+    };
+}
