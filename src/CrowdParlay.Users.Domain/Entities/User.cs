2d6b119fddb65322e3d469172c12602233a9a01d
maxaytt <Maxaytt@gmail.com>
2023-11-10 15:28:26 +0100

    feat(domain): add email to user entity

--- src/CrowdParlay.Users.Domain/Entities/User.cs
+++ src/CrowdParlay.Users.Domain/Entities/User.cs
@@ -6,6 +6,7 @@ public class User : EntityBase<Uuid>
 {
     public required string Username { get; set; }
     public required string DisplayName { get; set; }
+    public required string Email { get; set; }
     public required string? AvatarUrl { get; set; }
     public required string PasswordHash { get; set; }
     public required DateTimeOffset CreatedAt { get; set; }
