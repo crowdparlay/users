0216041244228f41a8f9dd58747e7dce42f0f464
maxaytt <Maxaytt@gmail.com>
2023-11-10 15:31:29 +0100

    feat(dtos): add email to usersDTOs

--- src/CrowdParlay.Users.Api/v1/DTOs/UsersDTOs.cs
+++ src/CrowdParlay.Users.Api/v1/DTOs/UsersDTOs.cs
@@ -3,12 +3,14 @@ namespace CrowdParlay.Users.Api.v1.DTOs;
 public record UsersRegisterRequest(
     string Username,
     string DisplayName,
+    string Email,
     string Password,
     string? AvatarUrl);
 
 public record UsersUpdateRequest(
     string? Username,
     string? DisplayName,
+    string? Email,
     string? AvatarUrl,
     string? OldPassword,
     string? NewPassword);
