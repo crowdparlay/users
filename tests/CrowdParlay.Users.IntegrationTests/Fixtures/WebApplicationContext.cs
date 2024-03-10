d6a61ef328e3e766ecc852557b0f7339221b3b58
Степной ишак <69521267+undrcrxwn@users.noreply.github.com>
2024-03-10 16:14:24 +0300

    tests: add users gRPC service tests

--- tests/CrowdParlay.Users.IntegrationTests/Fixtures/WebApplicationContext.cs
+++ tests/CrowdParlay.Users.IntegrationTests/Fixtures/WebApplicationContext.cs
@@ -1,5 +1,6 @@
 using CrowdParlay.Users.Api;
 using CrowdParlay.Users.IntegrationTests.Services;
+using Grpc.Net.Client;
 using MassTransit.Testing;
 using Nito.AsyncEx;
 using Testcontainers.PostgreSql;
@@ -11,7 +12,9 @@ namespace CrowdParlay.Users.IntegrationTests.Fixtures;
 // ReSharper disable once ClassNeverInstantiated.Global
 public class WebApplicationContext
 {
+    public readonly IServiceProvider Services;
     public readonly HttpClient Client;
+    public readonly GrpcChannel GrpcChannel;
     public readonly ITestHarness Harness;
 
     public WebApplicationContext()
@@ -25,6 +28,8 @@ public class WebApplicationContext
 
         var webApplicationFactory = new TestWebApplicationFactory<Program>(postgres.GetConnectionString());
         Client = webApplicationFactory.CreateClient();
+        GrpcChannel = GrpcChannel.ForAddress(Client.BaseAddress!, new GrpcChannelOptions { HttpClient = Client });
         Harness = webApplicationFactory.Services.GetTestHarness();
+        Services = webApplicationFactory.Services;
     }
 }
---
+++ tests/CrowdParlay.Users.IntegrationTests/Tests/UsersGrpcServiceTests.cs
@@ -0,0 +1,90 @@
+using CrowdParlay.Users.Application.Features.Users.Commands;
+using CrowdParlay.Users.gRPC;
+using CrowdParlay.Users.IntegrationTests.Fixtures;
+using FluentAssertions;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Mediator;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrowdParlay.Users.IntegrationTests.Tests;
+
+public class UsersGrpcServiceTests : IClassFixture<WebApplicationContext>
+{
+    private readonly IServiceProvider _services;
+    private readonly GrpcChannel _channel;
+
+    public UsersGrpcServiceTests(WebApplicationContext context)
+    {
+        _services = context.Services;
+        _channel = context.GrpcChannel;
+    }
+
+    [Fact(DisplayName = "Get user by ID")]
+    public async Task GetUserById()
+    {
+        // Arrange
+        await using var scope = _services.CreateAsyncScope();
+        var mediator = scope.ServiceProvider.GetRequiredService<ISender>();
+
+        var user = await mediator.Send(new Register.Command(
+            username: "john_doe",
+            displayName: "John Doe",
+            email: "johndoe@example.com",
+            avatarUrl: null,
+            password: "Test_Pa55w0RD!"));
+
+        // Act
+        var client = new UsersService.UsersServiceClient(_channel);
+        var request = new GetUserRequest { Id = user.Id.ToString() };
+        var response = await client.GetUserAsync(request);
+
+        // Assert
+        response.Should().BeEquivalentTo(new User
+        {
+            Id = user.Id.ToString(),
+            Username = user.Username,
+            DisplayName = user.DisplayName,
+            AvatarUrl = user.AvatarUrl
+        });
+    }
+
+    [Fact(DisplayName = "Get users by IDs")]
+    public async Task GetUsersByIds()
+    {
+        // Arrange
+        await using var scope = _services.CreateAsyncScope();
+        var mediator = scope.ServiceProvider.GetRequiredService<ISender>();
+
+        var users = new List<Register.Response>();
+        for (var i = 0; i < 300; i++)
+        {
+            var user = await mediator.Send(new Register.Command(
+                username: Guid.NewGuid().ToString("N")[..25],
+                displayName: "John Doe",
+                email: $"john{i}@example.com",
+                avatarUrl: null,
+                password: "Test_Pa55w0RD!"));
+
+            users.Add(user);
+        }
+
+        var firstUsers = users.Take(120).ToArray();
+        var firstUsersIds = firstUsers.Select(user => user.Id.ToString());
+        var expectedResponse = firstUsers.Select(user => new User
+        {
+            Id = user.Id.ToString(),
+            Username = user.Username,
+            DisplayName = user.DisplayName,
+            AvatarUrl = user.AvatarUrl
+        });
+
+        // Act
+        var client = new UsersService.UsersServiceClient(_channel);
+        var request = new GetUsersRequest { Ids = { firstUsersIds } };
+        var actualResponse = client.GetUsers(request).ResponseStream.ReadAllAsync().ToBlockingEnumerable();
+
+        // Assert
+        actualResponse.Should().BeEquivalentTo(expectedResponse);
+    }
+}
